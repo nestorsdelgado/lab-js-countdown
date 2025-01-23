6b52393923fcb9ec6c365f0c20d50dd479d161ef
Nestor Suarez Delgado <nestorsdelgado@gmail.com>
2025-01-23 13:03:24 +0000

    lab solution - Néstor

--- src/index.js
+++ src/index.js
@@ -7,16 +7,57 @@ let timer = null; // Variable to store the interval
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
+const countDown = document.querySelector("#start-btn");
 
+const toast = document.querySelector("#toast");
 
+const closeToast = document.querySelector("#close-toast");
+
+const toastMessage = document.querySelector("#toast-message");
+
+countDown.addEventListener("click", () => {
+
+  startCountdown();
+});
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
 
-
   // Your code goes here ...
+
+  countDown.disabled = true
+
+  let temporizador = document.querySelector("#time")
+
+  let i = 10;
+  const contador = setInterval(function () {
+
+    if(i == 10) {
+      let mensaje = "⏰ Final countdown! ⏰"
+      showToast(mensaje)
+    }
+
+    if(i == 5) {
+      let mensaje = "Start the engines! 💥"
+      showToast(mensaje)
+    }
+
+    if (i > 0) {
+      console.log(i)
+      temporizador.innerHTML = i
+    } else {
+      console.log(i)
+      temporizador.innerHTML = i
+      clearInterval(contador)
+      let mensaje = "Lift off! 🚀"
+      showToast(mensaje)
+    }
+
+    i--
+
+  }, 1000)
 }
 
 
@@ -28,11 +69,29 @@ function showToast(message) {
 
   // Your code goes here ...
 
+  //toast.setAttribute("class", "toast show") // -> OTRA MANERA DE CAMBIAR LA CLASE DEL BOTÓN (DEJO LA QUE TENÍA Y AÑADO "show")
 
+  toastMessage.innerHTML = message
+
+  const toastButton = toast.classList
+  toastButton.add("show")
+
+  const hideToast = setInterval(function () {
+
+    toastButton.remove("show")
+    clearInterval(hideToast)
+
+  }, 3000)
 
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
+
+  closeToast.addEventListener("click", () => {
+
+    toastButton.remove("show")
+
+  });
 
 }
